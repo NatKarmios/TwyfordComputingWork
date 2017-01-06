a86c0a1420b80d10aa3f006445fb13156eb72f44
Nat <nat@karmios.com>
2017-01-06 10:47:30 +0000

    Cleaned code in CollegeRegistration

--- src/com/karmios/nat/computingwork/utils/Utils.java
+++ src/com/karmios/nat/computingwork/utils/Utils.java
@@ -5,13 +5,12 @@ import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.*;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
-@SuppressWarnings({"WeakerAccess", "unused"})
+@SuppressWarnings({"WeakerAccess", "unused", "SameParameterValue"})
 public final class Utils {
     private static final Scanner sc = new Scanner(System.in);
     public static final Random rng = new Random();
