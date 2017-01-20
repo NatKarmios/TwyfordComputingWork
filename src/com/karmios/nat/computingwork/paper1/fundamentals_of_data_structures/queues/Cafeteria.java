2278ea7dd339ad0b9ad3d4ebb8cefa31a5a36343
Nat <nat@karmios.com>
2017-01-20 11:01:50 +0000

    Added to Cafeteria.java

--- src/com/karmios/nat/computingwork/paper1/fundamentals_of_data_structures/queues/Cafeteria.java
+++ src/com/karmios/nat/computingwork/paper1/fundamentals_of_data_structures/queues/Cafeteria.java
@@ -1,22 +1,28 @@
 package com.karmios.nat.computingwork.paper1.fundamentals_of_data_structures.queues;
 
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 import static com.karmios.nat.computingwork.utils.Utils.*;
 
 public class Cafeteria {
+    private static AtomicInteger count = new AtomicInteger(0);
+    private static Queue<String> queue;
     public static void main(String[] args) {
-        Queue<Integer> queue = populateQueue();
+        queue = populateQueue();
         Till[] tills = {
                 new Till(queue, 1000),
                 new Till(queue, 1000),
                 new Till(queue, 1000),
                 new Till(queue, 2000)
         };
-
+        System.out.println(" == Press enter to add a new student to the queue == ");
         Arrays.stream(tills).map(till -> till.thread).forEach(Thread::start);
+
+        QueueAdder.start();
+
         Arrays.stream(tills).forEach(till -> {
             try {
                 till.thread.join();
@@ -28,11 +34,37 @@ public class Cafeteria {
         System.out.printf("Queue size: %s\n", queue.size());
     }
 
-    private static Queue<Integer> populateQueue() {
-        int numOfStudents = randInt(500, 750);
-        Queue<Integer> queue = new Queue<>();
-        IntStream.range(0, numOfStudents).forEach(queue::put);
+    private static Queue<String> populateQueue() {
+        int numOfStudents;
+        // numOfStudents = randInt(500, 750);
+        numOfStudents = inputIntLoop("How many students start in the queue? => ",
+                "Can't have a negative number of students!", x -> x>=0);
+        Queue<String> queue = new Queue<>();
+        IntStream.range(0, numOfStudents).forEach(x -> queue.put("(initial) "+count.getAndIncrement()));
         return queue;
+    }
+
+    // Daemon class for adding to the queue.
+    private static class QueueAdder implements Runnable {
+        Scanner sc;
+        QueueAdder() {
+            sc = new Scanner(System.in);
+        }
+
+        @Override
+        public void run() {
+            //noinspection InfiniteLoopStatement
+            while (true) {
+                sc.nextLine();
+                queue.put("(post-init) "+count.getAndIncrement());
+            }
+        }
+
+        private static void start() {
+            Thread adderDaemonThread = new Thread(new QueueAdder());
+            adderDaemonThread.setDaemon(true);
+            adderDaemonThread.start();
+        }
     }
 }
 
@@ -40,11 +72,11 @@ class Till implements Runnable {
     private static transient AtomicInteger tillCount = new AtomicInteger(0);
     final int id;
     private final long waitTime;
-    private Queue<Integer> queue;
+    private Queue queue;
 
     final Thread thread;
 
-    Till(Queue<Integer> queue, long waitTime) {
+    Till(Queue queue, long waitTime) {
         this.id = tillCount.getAndIncrement();
         this.queue = queue;
         this.waitTime = waitTime;
@@ -54,7 +86,7 @@ class Till implements Runnable {
     @Override
     public void run() {
         try {
-            Integer student_id;
+            Object student_id;
             while ((student_id = queue.get()) != null) {
                 Thread.sleep(waitTime);
                 System.out.printf(" Till %s served student %s.\n", id, student_id);
