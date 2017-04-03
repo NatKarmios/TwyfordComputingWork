af21604f5effcde1f290350e230fc35c0be6ae27
Nat <nat@karmios.com>
2017-04-03 01:38:52 +0100

    Finished 20th challenge.

---
+++ src/com/karmios/nat/computingwork/ocr_challenges/Challenge34_WhatsTheDay.java
@@ -0,0 +1,20 @@
+package com.karmios.nat.computingwork.ocr_challenges;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
+import static com.karmios.nat.computingwork.utils.Utils.inputIntLoop;
+
+public class Challenge34_WhatsTheDay {
+    public static void main(String[] args) {
+        try {
+            LocalDate date = LocalDate.of(inputIntLoop("Year: "), inputIntLoop("Month: "),
+                    inputIntLoop("Day: "));
+            System.out.printf("%s is a %s.\n", date.toString(), date.getDayOfWeek());
+        } catch (DateTimeException e) {
+            System.out.println("Invalid date!");
+        }
+    }
+
+
+}
