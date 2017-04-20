2cdb9c5e216f7c8e620d28305656a98cf4f7e6c9
Nat <nat@karmios.com>
2017-04-20 11:39:13 +0100

    Tried out Unit Tests

---
+++ src/com/karmios/nat/computingwork/ocr_challenges/Challenge15_Pangrams_Test.java
@@ -0,0 +1,33 @@
+package com.karmios.nat.computingwork.ocr_challenges;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static com.karmios.nat.computingwork.ocr_challenges.Challenge15_Pangrams.isPangram;
+
+class Challenge15_Pangrams_Test {
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    @Test
+    void isPangramReturnsTrueForAlphabet() {
+        assertTrue(isPangram(ALPHABET));
+    }
+
+    @Test
+    void isPangramReturnsFalseForNonPangram() {
+        assertFalse(isPangram("notapangram"));
+    }
+
+    @Test
+    void isPangramIgnoresNonLetters() {
+        assertTrue(isPangram(ALPHABET+"!.?:;<>[]{}@'\""));
+    }
+
+    @Test
+    void isPangramIgnoresCase() {
+        assertTrue(isPangram("AbCdEfGhIjKlMnOpQrStUvWxYz"));
+    }
+
+
+}
