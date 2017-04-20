a78305876a04b453c305995c5cf11e8708fed045
Nat <nat@karmios.com>
2017-04-20 11:38:35 +0100

    Tried out Unit Tests

--- src/com/karmios/nat/computingwork/ocr_challenges/Challenge15_Pangrams.java
+++ src/com/karmios/nat/computingwork/ocr_challenges/Challenge15_Pangrams.java
@@ -7,7 +7,10 @@ import java.util.stream.IntStream;
 public class Challenge15_Pangrams {
     public static void main(String[] args) {
         String input = Utils.input("Enter a phrase: ");
-        System.out.printf("\"%s\" is%s a pangram.", input, (IntStream.range(65, 91)
-                .allMatch(x -> input.toUpperCase().contains(Character.toString((char) x)))) ? "" : " not");
+        System.out.printf("\"%s\" is%s a pangram.", input, isPangram(input) ? "" : " not");
+    }
+
+    static boolean isPangram(String str) {
+        return IntStream.range(65, 91).allMatch(x -> str.toUpperCase().contains(Character.toString((char) x)));
     }
 }
